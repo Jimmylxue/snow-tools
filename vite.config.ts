65ca3a04919f66253e4d1c45569b3d21ab278aa2
jimmyxuexue <1002661758@qq.com>
2024-12-02 17:12:58 +0800

    alias 更新

--- vite.config.ts
+++ vite.config.ts
@@ -29,51 +29,8 @@ export default defineConfig({
 		}),
 	],
 	resolve: {
-		alias: [
-			{
-				find: /^~/,
-				replacement: '',
-			},
-			{
-				find: '@/types',
-				replacement: resolve(__dirname, 'src/types'),
-			},
-			{
-				find: '@/utils',
-				replacement: resolve(__dirname, 'src/utils'),
-			},
-			{
-				find: '@/hooks',
-				replacement: resolve(__dirname, 'src/hooks'),
-			},
-			{
-				find: '@/stores',
-				replacement: resolve(__dirname, 'src/stores'),
-			},
-			{
-				find: '@/core',
-				replacement: resolve(__dirname, 'src/core'),
-			},
-			{
-				find: '@/components',
-				replacement: resolve(__dirname, 'src/components'),
-			},
-			{
-				find: '@/config',
-				replacement: resolve(__dirname, 'src/config'),
-			},
-			{
-				find: '@/api',
-				replacement: resolve(__dirname, 'src/api'),
-			},
-			{
-				find: '@/pages',
-				replacement: resolve(__dirname, 'src/pages'),
-			},
-			{
-				find: '@/assets',
-				replacement: resolve(__dirname, 'src/assets'),
-			},
-		],
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
 	},
 })
