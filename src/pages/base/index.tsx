4883c5ba18d660a860393b5e6a346e36c9d7d57b
jimmyxuexue <1002661758@qq.com>
2024-12-02 17:25:59 +0800

    style: 新增shadcnUI

--- src/pages/base/index.tsx
+++ src/pages/base/index.tsx
@@ -1,5 +1,15 @@
 const url = import.meta.env.VITE_APP_API_BASE_URL
+import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react'
 
 export function Base() {
-	return <div className=" text-red-400">hello base {url}</div>
+	return (
+		<>
+			<div className=" text-red-400">hello base {url}</div>
+			<Button disabled>
+				<Loader2 className="animate-spin" />
+				Please wait
+			</Button>
+		</>
+	)
 }
